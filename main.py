5cfa3aa9ff4c62eb9efa362b54dcdfa51a48efed
Hubert <hubert2recovery1@outlook.com>
2026-01-20 19:05:34 +0000

    Commit to pull & continue from PyCharm

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import importlib # Used for importing the modules from the modules folder
-import os # Used for listing modules in the modules folder
-import time # Used for adding delays
+import importlib  # Used for importing the modules from the modules folder
+import os  # Used for listing modules in the modules folder
+import time  # Used for adding delays
+import getpass  # Used for hiding password input
 
-
-modules_loaded = {} # Hold dictionaries from registered commands
+modules_loaded = {}  # Hold dictionaries from registered commands
 commands = {}
 
-def boot():
-    print("Welcome to LumenOS!")
+def loginscreen():
+    print("Welcome to LumenOS Secure Login\n")
+    username = input("Username: ")
+    password = getpass.getpass("Password: ")
+    # For demonstration purposes, we accept any username/password
+    print(f"\n[INFO]Login successful. Welcome, {username}!\n")
     time.sleep(1)
-    for module in os.listdir('./modules'): #  Lists all files in the modules folder
-        if module.startswith('LOS_') and module.endswith('.py'):
+
+def boot():
+    print("Welcome to LumenOS!\n\n[INFO]Loading modules...")
+    time.sleep(1)
+    for module in os.listdir('./modules'):  # Lists all files in the modules folder
+        if module.startswith('LOS') and module.endswith('.py'):
             module_to_load = module[:-3]  # Removing the .py at the end
             try:
                 mod = importlib.import_module('modules.' + module_to_load)
@@ -21,7 +29,7 @@ def boot():
                     print(f'[FAIL]Module {module_to_load} does not have a register_command function.')
             except Exception as e:
                 print(f'[FAIL]Failed to load module: {module_to_load}. {e}')
-        time.sleep(0.2)  # Small delay for better readability
+        time.sleep(0.05)  # Small delay for better readability
     print(f'\n\n[INFO]Total modules loaded: {len(commands)}')
     time.sleep(2)  # Delay for 2 seconds before starting main loop
     main()
@@ -32,6 +40,14 @@ def main():
         if len(commands_input) == 0:
             pass  # If no input is given, do nothing
         else:
-            print(commands_input)
+            command_to_run = commands_input[0]  # First item in list is the command
+            arguments = commands_input[1:]  # The rest are arguments
+            if command_to_run in commands:  # Checks if command exists in commands dictionary
+                try:
+                    commands[command_to_run]['func'](arguments, commands)  # Runs the main function of the command
+                except TypeError:
+                    commands[command_to_run]['func'](arguments)  # Runs the main function of the command
+                except Exception as e:
+                    print(f'[ERROR]An error occurred while executing the command: {e}')
 
 boot()
---
+++ modules/LOS_version.py
@@ -0,0 +1,12 @@
+def register_command(commands):
+    commands['version'] = {
+        'brief': 'displays the current version of LumenOS',
+        'detailed': '''Displays the current version of LumenOS.
+
+        Usage
+        version''',
+        'func': version_main
+    }
+
+def version_main():
+    print("LumenOS v0.5.2 Alpha")
