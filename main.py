11d10c00fa33623c1e96f3c11e8678379bb33136
Hubert <hubert2recovery1@outlook.com>
2026-01-22 09:04:11 +0000

    Fix wording for system messages.

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def loginscreen():
     username = input("Username: ")
     password = getpass.getpass("Password: ")
     # For demonstration purposes, we accept any username/password
-    print(f"\n[INFO]Login successful. Welcome, {username}!\n")
+    print(f"\n[INFRM]  Login successful. Welcome, {username}!\n")
     time.sleep(1)
 
 def boot():
-    print("Welcome to LumenOS!\n\n[INFO]Loading modules...")
+    print("Welcome to LumenOS!\n\n[INFRM]  Loading modules...")
     time.sleep(1)
     for module in os.listdir("./modules"):  # Lists all files in the modules folder
         if module.startswith("LOS") and module.endswith(".py"):
@@ -24,27 +24,27 @@ def boot():
                 mod = importlib.import_module("modules." + module_to_load)
                 if hasattr(mod, "register_command"):  # Checks if python file has module loader
                     mod.register_command(commands)  # Runs register command (adds help prompts and main function to the commands dictionary)
-                    print(f"[ OK ]Loaded module: {module_to_load}")
+                    print(f"[ OK! ]  Loaded module: {module_to_load}")
                 else:
-                    print(f"[FAIL]Module {module_to_load} does not have a register_command function.")
+                    print(f"[ERROR]  Module {module_to_load} does not have a register_command function.")
             except Exception as e:
-                print(f"[FAIL]Failed to load module: {module_to_load}. {e}")
+                print(f"[ERROR]  ERRORed to load module: {module_to_load}. {e}")
         time.sleep(0.05)  # Small delay for better readability
-    print(f"[INFO]Total modules loaded: {len(commands)}\n\n")
+    print(f"[INFRM]  Total modules loaded: {len(commands)}\n\n")
 
     try:
-        print("[INFO]Attempting to load config...")
+        print("[INFRM]  Attempting to load config...")
         time.sleep(0.5)
         config = commands["load"]["func"]
-        print("[ OK ]Config loaded successfully!")
+        print("[ OK! ]  Config loaded successfully!")
     except Exception as e:
-        print(f"[FAIL]Failed to load config: {e}")
+        print(f"[ERROR]  ERRORed to load config: {e}")
     time.sleep(2)  # Delay for 2 seconds before starting main loop
     main()
 
 def main():
     while True:
-        commands_input = input("LumenOS> ").strip().split()  # Gets user input and splits it into a list
+        commands_input = input("\nLumenOS> ").strip().split()  # Gets user input and splits it into a list
         if len(commands_input) == 0:
             pass  # If no input is given, do nothing
         else:
@@ -56,6 +56,8 @@ def main():
                 except TypeError:
                     commands[command_to_run]["func"](arguments)  # Runs the main function of the command
                 except Exception as e:
-                    print(f"[ERROR]An error occurred while executing the command: {e}")
+                    print(f"[ERROR]  An error occurred while executing the command: {e}")
+            else:
+                print(f"[ERROR]  Command '{command_to_run}' not found. Type 'help' to see available commands.")
 
 boot()
